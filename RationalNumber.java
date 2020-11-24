7eb44390bc1ca3add761bc4e025b05d2ba18dac9
emmy-chen <echen20@stuy.edu>
2020-11-24 08:13:51 -0500

    added RationalNumber() method

---
+++ RationalNumber.java
@@ -0,0 +1,12 @@
+public class RationalNumber extends RealNumber {
+  private int numerator, denominator;
+
+  public RationalNumber(int nume, int deno){
+    super(0.0);
+    numerator = nume;
+    denominator = deno;
+  }
+
+
+
+}
